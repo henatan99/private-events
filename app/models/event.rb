5dea05d3df0103c8d754079b05ece132467f5fd8
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-27 15:19:20 +0200

    add assocaitions in user and event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :event_creatings, foreign_key: :created_event_id
+  has_many :attendees, through: :attended_event, source: :event_creator
+  belongs_to :creator, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :events, foreign_key: :user_id
+  has_many :created_events, through: :event_creating
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
 end
