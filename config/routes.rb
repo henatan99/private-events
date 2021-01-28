a021929afb63fbea21529d8bc9525dd6226767b1
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-28 16:34:48 +0200

    style events home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create destroy]
   resources :sessions, only: %i[new create destroy]
-  resources :events, only: %i[new create index]
+  resources :events, only: %i[new create index destroy edit]
   root to: 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
